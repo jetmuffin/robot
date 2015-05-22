e6e6cb673bb76ff54be1a5eebdd92c6589ef294f
sunzequn <sunzequn@live.cn>
2015-05-22 18:14:03 +0800

    merge

---
+++ src/main/java/com/dag/robot/data/input/InputFromJson.java
@@ -0,0 +1,11 @@
+package com.dag.robot.data.input;
+
+import org.codehaus.jettison.json.JSONObject;
+
+public class InputFromJson {
+	
+	private String expertFilePath;
+	private String paperFilePath;
+	private String patentFilePath;
+	
+}
--- src/main/java/com/dag/robot/utils/PropertiesUtil.java
+++ src/main/java/com/dag/robot/utils/PropertiesUtil.java
@@ -11,7 +11,7 @@ public class PropertiesUtil {
 	public static String getValue(String key){
 		Properties mProperties = new Properties();
 		try {
-			mProperties.load(PropertiesUtil.class.getClassLoader().getResourceAsStream("config/config.properties"));
+			mProperties.load(PropertiesUtil.class.getClassLoader().getResourceAsStream("config.properties"));
 			return mProperties.getProperty(key);
 		} catch (IOException e) {
 			e.printStackTrace();
